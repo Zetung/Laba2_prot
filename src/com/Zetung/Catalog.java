67f884b651e136ce62e0d987b89adae93c53274b
Zetung <79874835+Zetung@users.noreply.github.com>
2021-10-19 12:48:47 +0300

    Initial commit

---
+++ src/com/Zetung/Catalog.java
@@ -0,0 +1,28 @@
+package com.Zetung;
+
+import java.util.List;
+
+/**
+ * The type Catalog.
+ */
+public class Catalog {
+    private List<IProducts> productsList;
+
+    /**
+     * Instantiates a new Catalog.
+     *
+     * @param productsList the products list
+     */
+    public Catalog(List<IProducts> productsList) {
+        this.productsList = productsList;
+    }
+
+    /**
+     * Gets products list.
+     *
+     * @return the products list
+     */
+    public List<IProducts> getProductsList() {
+        return productsList;
+    }
+}
